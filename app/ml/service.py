334d26aafe701b52c904010bbe5b222da1f6d525
Nurzhigit <askaraliyev.taraz@gmail.com>
2026-09-04 15:31:46 +0500

    Test CI regression detection

--- app/ml/service.py
+++ app/ml/service.py
@@ -64,7 +64,8 @@ class ModelService:
 
         probability = float(self.model.predict_proba(x)[0, 1])
 
-        risk_class = int(probability >= self.threshold)
+        # risk_class = int(probability >= self.threshold)
+        risk_class = 7
 
         return probability, risk_class
 
